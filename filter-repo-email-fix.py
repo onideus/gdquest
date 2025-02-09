e25c5c14a50ed6e3b2b6a2edba367ca755634c39
Zach Martin <zach@zach-martin.com>
2025-02-08 23:19:31 -0500

    Setting up a better structure for tracking my progress, as well as sharing my notes for future me.

--- filter-repo-email-fix.py
+++
@@ -1,8 +0,0 @@
-from git_filter_repo import RepoFilter
-
-def rewrite_email(commit):
-	if commit.author_email == "zachmartin@zachs-macbook-pro.local":
-		commit.author_email = "zach@zach-martin.com"
-		commit.committer_email = "zach@zach-martin.com"
-
-RepoFilter(rewrite_email).run()
--- rewrite-email.py
+++
@@ -1,4 +0,0 @@
-def commit_callback(commit):
-    if commit.author_email == "zachmartin@zachs-macbook-pro.local":
-        commit.author_email = "zach@zach-martin.com"
-        commit.committer_email = "zach@zach-martin.com"
