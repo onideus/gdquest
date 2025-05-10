9d4d5a7c45ec850dca1cd7d879d548fd6aa707c4
Zach Martin <zach@zach-martin.com>
2025-05-10 19:53:22 -0400

    Completed Lesson 5 and practice questions.

--- README.md
+++ README.md
@@ -76,7 +76,7 @@ Below is an indicative list of modules based on the course outline. I will updat
   - [x] L2. Create the chest
   - [x] L3. The outline thickens
   - [x] L4. The chest open animation
-  - [ ] L5. Open the chest
+  - [x] L5. Open the chest
   - [ ] L6. Spawn random loot
   - [ ] L7. Animate the collectibles
   - [ ] L8. The programmer's dictionary
--- modules/M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
+++ modules/M06.looting_workbook/practices/L5.P1.open_the_door/door.gd
@@ -2,12 +2,17 @@ extends Area2D
 
 @onready var animation_player: AnimationPlayer = $"AnimationPlayer"
 
-
 func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
 	# Complete this boolean expression to check if the input event is a left-mouse click.
 	var event_is_mouse_click: bool = (
-		false
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.is_pressed()
 	)
 
 	if event_is_mouse_click:
-		pass
+		open()
+
+func open() -> void: 
+	input_pickable = false
+	animation_player.play("open")
